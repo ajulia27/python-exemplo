55a3feafa49a02c2ae3649ed374da6657d792b4a
ajulia27 <167909420+ajulia27@users.noreply.github.com>
2024-05-21 15:08:26 -0300

    criação lista.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Ana Julia", 4, 8.5, True]
+print(f"nome: {aluno[0]}")
+print(f"falta: {aluno[1]}")
+print(f"media: {aluno[2]}")
+if aluno [3] == True:
+     print(f"aprovado: sim")
+else:
+     print(f"aprovado: não")
